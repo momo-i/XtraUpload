afe343cd670ae52e1b870abe6a5ae3772878cec2
momo-i <webmaster@momo-i.org>
2015-01-14 21:42:07 +0900

    update charts

--- application/views/default/api/charts/all_server_used_space.php
+++ application/views/default/api/charts/all_server_used_space.php
@@ -1,9 +1,20 @@
 $('#chart_title').html('<?php echo $title; ?>');
-
+var chart = c3.generate({
+  bindto: '#chart_data',
+  data: {
+    columns: [
 <?php
 foreach($servers as $server)
 {
-
+?>
+      ['<?php echo $server['name']; ?>', '<?php echo $server['num']; ?>'],
+<?php
 }
 ?>
-$('#chart_data').html('<strong>Nothing to do</strong>');
+    ],
+    type: 'pie',
+  },
+  legend: {
+    show: true
+  }
+});
