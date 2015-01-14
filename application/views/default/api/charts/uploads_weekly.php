9096de83d861bb135b8b264a538ea49ecabfef9b
momo-i <webmaster@momo-i.org>
2015-01-14 11:47:21 +0900

    update charts

--- application/views/default/api/charts/uploads_weekly.php
+++ application/views/default/api/charts/uploads_weekly.php
@@ -1,6 +1,28 @@
-<?php
-$rand = mt_rand();
-?>
-var chart<?php echo $rand; ?> = new FusionCharts("<?php echo base_url(); ?>assets/flash/charts/Column3D.swf", "ChartId", "<?php echo $height; ?>", "<?php echo $width; ?>", "0", "0");
-chart<?php echo $rand; ?>.setDataXML("<chart caption='<?php echo lang('Past 7 Days Uploads'); ?>' xAxisName='<?php echo lang('Day'); ?>' yAxisName='<?php echo lang('Uploads'); ?>' showValues='0' decimals='0' formatNumberScale='0'><set label='<?php $d='-6'; echo date('D', strtotime($d.' days')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>'/><set label='<?php $d='-5'; echo date('D', strtotime($d.' days')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>'/><set label='<?php $d='-4'; echo date('D', strtotime($d.' days')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>'/><set label='<?php $d='-3'; echo date('D', strtotime($d.' days')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>'/><set label='<?php $d='-2'; echo date('D', strtotime($d.' days')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>'/><set label='<?php $d='-1'; echo date('D', strtotime($d.' days')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>'/><set label='<?php $d='-6'; echo date('D', strtotime('today')); ?>' value='<?php echo $this->db->get_where('refrence', array('time >' => strtotime('today 12:00 AM'), 'time <' => strtotime('today 11:59:59 PM')))->num_rows(); ?>'/></chart>");
-chart<?php echo $rand; ?>.render("chart_data");
+$('#chart_title').html('<?php echo lang('Past 7 Days Uploads'); ?>');
+var chart = c3.generate({
+  bindto: '#chart_data',
+  data: {
+    columns: [
+      ['<?php $d = '-6'; echo date('Y-m-d', strtotime($d.' days')); ?>', <?php echo $this->db ->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>],
+      ['<?php $d = '-5'; echo date('Y-m-d', strtotime($d.' days')); ?>', <?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>],
+      ['<?php $d = '-4'; echo date('Y-m-d', strtotime($d.' days')); ?>', <?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>],
+      ['<?php $d = '-3'; echo date('Y-m-d', strtotime($d.' days')); ?>', <?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>],
+      ['<?php $d = '-2'; echo date('Y-m-d', strtotime($d.' days')); ?>', <?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>],
+      ['<?php $d = '-1'; echo date('Y-m-d', strtotime($d.' days')); ?>', <?php echo $this->db->get_where('refrence', array('time >' => strtotime($d.' days 12:00 AM'), 'time <' => strtotime($d.' days 11:59:59 PM')))->num_rows(); ?>],
+      ['<?php echo date('Y-m-d', strtotime('today')); ?>',<?php echo $this->db->get_where('refrence', array('time >' => strtotime('today 12:00 AM'), 'time <' => strtotime('today 11:59:59 PM')))->num_rows(); ?>]
+    ],
+    type: 'bar',
+  },
+  axis: {
+    x: {
+      show: false,
+      type: 'categorized'
+    }
+  },
+  bar: {
+    width: {
+      ratio: 0.3,
+    },
+  }
+});
+
