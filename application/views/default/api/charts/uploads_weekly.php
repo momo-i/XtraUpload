733155365326996d813a5dba9b4f14e22ef10fb3
momo-i <webmaster@momo-i.org>
2015-01-16 09:49:30 +0900

    update charts

--- application/views/default/api/charts/uploads_weekly.php
+++ application/views/default/api/charts/uploads_weekly.php
@@ -7,7 +7,9 @@ var chart = c3.generate({
       ['x', '<?php echo $day6['d']; ?>', '<?php echo $day5['d']; ?>', '<?php echo $day4['d']; ?>', '<?php echo $day3['d']; ?>', '<?php echo $day2['d']; ?>', '<?php echo $day1['d']; ?>', '<?php echo $today['d']; ?>'],
       ['<?php echo lang('All uploads'); ?>', <?php echo $day6['num']; ?>, <?php echo $day5['num']; ?>, <?php echo $day4['num']; ?>, <?php echo $day3['num']; ?>, <?php echo $day2['num']; ?>, <?php echo $day1['num']; ?>, <?php echo $today['num']; ?>]
     ],
-    groups: [['<?php echo lang('All uploads'); ?>']],
+    groups: [
+      ['<?php echo lang('All uploads'); ?>']
+    ],
     type: 'bar',
   },
   axis: {
